9771b363a128d08c5124acde83f8ee5e759bd343
Joy Siang <sianglianmang2011@gmail.com>
2022-09-13 22:50:08 +0900

    feat: props method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-class Item extends React.Component{
-  render(){
-    return(
+class Item extends React.Component {
+  render() {
+    return (
       <li>
         {this.props.name},
         ${this.props.price}
@@ -9,45 +9,60 @@ class Item extends React.Component{
     )
   }
 }
-class App extends React.Component {
-  state = {
-    items:[
-      {id:1, name:'Apple', price:0.95},
-      {id:2, name:'Orange', price:0.89},
-    ]
-  }
-
+class AddForm extends React.Component {
   nameRef = React.createRef();
   priceRef = React.createRef();
-
-  add = ()=>{
-    let id = this.state.items.length + 1;
+  add = () => {
     let name = this.nameRef.current.value;
     let price = this.priceRef.current.value;
+    this.props.add(name, price);
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.nameRef} /><br />
+        <input type="text" ref={this.priceRef} /><br />
+        <button onClick={this.add}>Add</button>
+      </div>
+    )
+  }
+}
 
+class App extends React.Component {
+  state = {
+    items: [
+      { id: 1, name: 'Apple', price: 0.99 },
+      { id: 2, name: 'Orange', price: 0.89 },
+    ]
+  }
+  add = (name, price) => {
+    let id = this.state.items.length + 1;
     this.setState({
       items: [
-          ...this.state.items,
-          {id, name, price }
+        ...this.state.items,
+        { id, name, price }
       ]
     });
   }
   render() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-      <ul>
-        {this.state.items.map(i=>{
-          return(
-            <Item key={i.id} name={i.name} price={i.price}/>
-          )
-        })}
-      </ul>
-      <input type="text" ref={this.nameRef} /><br />
-      <input type="text" ref={this.priceRef} /><br />
-      <button onClick={this.add}>Add</button>
-    </div>
-    );
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <ul>
+          {this.state.items.map(i => {
+            return (
+              <Item
+                id={i.id}
+                name={i.name}
+                price={i.price}
+              />
+            )
+          })}
+        </ul>
+        <AddForm add={this.add} />
+      </div>
+    )
   }
- }
- export default App;
+}
+
+export default App;
